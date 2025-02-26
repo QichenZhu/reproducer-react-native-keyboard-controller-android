6a10df13d5dbbec8b8fb97dec18b565075957552
QichenZhu <57348009+QichenZhu@users.noreply.github.com>
2025-02-26 19:41:00 +1300

    Update reproducer

--- App.tsx
+++ App.tsx
@@ -1,55 +1,58 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
-import {Text, TextInput, TouchableOpacity} from 'react-native';
-import {
-  KeyboardAvoidingView,
-  KeyboardProvider,
-} from 'react-native-keyboard-controller';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useRef } from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { KeyboardAvoidingView, KeyboardProvider } from 'react-native-keyboard-controller';
 
-const Screen1 = props => {
+function Dummy(): React.JSX.Element {
+  const data: number[] = [];
+  for (let i = 0; i < 20; i++) {
+    for (let j = 0; j < 100; j++) {
+      data.push(Math.random());
+    }
+  }
   return (
-    <KeyboardAvoidingView
-      behavior="padding"
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        backgroundColor: 'red',
-      }}>
-      <Text>Screen 1</Text>
-      <TouchableOpacity style={{backgroundColor: 'yellow', padding: 48}}>
-        <Text>TEST BUTTON</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => props.navigation.navigate('screen2')}
-        style={{backgroundColor: 'green', padding: 48}}>
-        <Text>Go to screen 2</Text>
-      </TouchableOpacity>
+    <View style={{ height: 100, overflow: 'hidden' }}>
+      {data.map((num, index) => (
+        <Text key={index} style={{textAlign: 'center'}}>{num}</Text>
+      ))}
+    </View>
+  );
+}
+
+function Screen1(props: any): React.JSX.Element {
+  const textInputRef = useRef<TextInput | null>(null);
+  return (
+    <KeyboardAvoidingView behavior="padding" style={{flex: 1}}>
+      <View style={{backgroundColor: 'grey', flex: 1}}>
+        <TextInput ref={textInputRef} placeholder="Show keyboard" style={{ backgroundColor: 'lightgrey', width: '100%', height: 100, textAlign: 'center' }} />
+        <TouchableOpacity
+          onPress={() => {
+            textInputRef.current?.blur();
+            props.navigation.replace('Screen2');
+          }}
+          style={{ backgroundColor: 'teal', width: '100%' }}>
+          <Text style={{ color: 'white', lineHeight: 100, textAlign: 'center' }}>Go to screen 2</Text>
+        </TouchableOpacity>
+      </View>
     </KeyboardAvoidingView>
   );
 };
 
-const Screen2 = props => {
+function Screen2(props: any): React.JSX.Element {
   return (
-    <KeyboardAvoidingView
-      behavior="padding"
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        backgroundColor: 'green',
-      }}>
-      <Text>Screen 2</Text>
-      <TextInput style={{backgroundColor: 'white', padding: 32, width: 100}} />
-      <TouchableOpacity
-        onPress={() => props.navigation.navigate('screen1')}
-        style={{backgroundColor: 'blue', padding: 48}}>
-        <Text>Go to screen 1</Text>
-      </TouchableOpacity>
+    <KeyboardAvoidingView behavior="padding" style={{flex: 1}}>
+      <View style={{backgroundColor: 'grey', flex: 1}}>
+        <Dummy />
+        <TouchableOpacity
+          onPress={() => props.navigation.replace('Screen1')}
+          style={{ backgroundColor: 'teal', width: '100%' }}>
+          <Text style={{ color: 'white', lineHeight: 100, textAlign: 'center' }}>Go to screen 1</Text>
+        </TouchableOpacity>
+      </View>
     </KeyboardAvoidingView>
   );
-};
+}
 
 const Stack = createNativeStackNavigator();
 
@@ -58,8 +61,8 @@ function App(): React.JSX.Element {
     <KeyboardProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="screen1" component={Screen1} />
-          <Stack.Screen name="screen2" component={Screen2} />
+          <Stack.Screen name="Screen1" component={Screen1} />
+          <Stack.Screen name="Screen2" component={Screen2} />
         </Stack.Navigator>
       </NavigationContainer>
     </KeyboardProvider>
